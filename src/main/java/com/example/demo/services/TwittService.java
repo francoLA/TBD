91e137a643a8fa9d20e078a7ed129bcedaf25ce3
Franco <franco.labra@usach.cl>
2019-05-29 01:01:51 -0400

    twitt service completed

--- src/main/java/com/example/demo/services/TwittService.java
+++ src/main/java/com/example/demo/services/TwittService.java
@@ -1,23 +1,55 @@
 package com.example.demo.services;
 
+import com.example.demo.elasticSearch.SearchRepository;
+import com.example.demo.kafka.TwitterKafkaProducer;
 import com.example.demo.models.Twitt;
 import com.example.demo.repositories.TwittRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/twitts")
+@RequestMapping(value = "/twitt")
 public class TwittService {
 
     @Autowired
     private TwittRepository twittRepository;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @Autowired
+    private SearchRepository searchRepository;
+
+    @Autowired
+    private TwitterKafkaProducer twitterKafkaProducer;
+
+    @RequestMapping(value = "/{id", method = RequestMethod.GET)
     @ResponseBody
-    public Iterable<Twitt> getAllTwitts(){
+    public Twitt getTwittById(@PathVariable String id){
+        return this.twittRepository.findTweetById(id);
+    }
+
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Twitt> getAllTwitts(){
+        return twittRepository.findAll();
+    }
+
+    @RequestMapping(value = "/startKafka", method = RequestMethod.GET)
+    public void start(){
+
+        List<String> hashtags = new ArrayList<>();
+
+        hashtags.add("isapre");
+        hashtags.add("consalud");
+        hashtags.add("salud");
+
+        this.twitterKafkaProducer.run(this.twittRepository, this.searchRepository, hashtags);
+    }
+
+    @RequestMapping(value = "/stopKafka", method = RequestMethod.GET)
+    public List<Twitt> stop(){
+        this.twitterKafkaProducer.stop();
         return twittRepository.findAll();
     }
 
